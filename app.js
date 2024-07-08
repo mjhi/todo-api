15b2c7c0d5ca9dda45367b28a2da69b697ff311c
mjhi <80163560+mjhi@users.noreply.github.com>
2024-07-08 16:35:59 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import mockTasks from './data/mock.js';
+import mongoose from 'mongoose';
+import Task from './models/Task.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// ...
+
+mongoose.connect(process.env.DATABASE_URL).then(() => console.log('Connected to DB'));
+
+const app = express();
+app.use(express.json());
+
+function asyncHandler(handler){
+    return async function (req,res) {
+        try {
+            await handler(req,res);
+        } catch(e)
+        {
+            if(e.name === 'ValidationError'){
+                res.status(400).send({message:e.message});
+            }else if(e.name === 'CastError'){
+                res.status(404).send({message:'Cannot find given id.'});
+            }else{
+                res.status(500).send({message:e.message});
+            }
+        }
+    }
+}
+app.get('/tasks', asyncHandler(async (req,res)=>{
+    const sort = req.query.sort;
+    const count = Number(req.query.count) || 0;
+
+
+    const sortOption = {
+        createAt: sort === 'oldest'? 'asc' : 'desc'
+    };
+    const tasks = await Task.find().sort(sortOption).limit(count);
+    res.send(tasks);
+}));
+
+app.get('/tasks/:id', asyncHandler(async (req,res)=>{
+    const id = req.params.id;
+    const task = await Task.findById(id);
+
+    if(task) {
+        res.send(task);
+    } else{
+        res.status(404).send({message : 'Cannot find given id,'});
+    }
+}));
+
+app.post('/tasks',asyncHandler(async (req,res)=>{
+    const newTask = await Task.create(req.body);
+    res.status(201).send(newTask);
+
+}));
+
+app.patch('tasks/:id', asyncHandler(async (req,res)=>{
+    const id = req.params.id;
+    const task = await Task.findById(id);
+    if(task) {
+        Object.keys(req.body).forEach((key)=>{
+            task[key] = req.body[key];
+        });
+        await task.save();
+        res.send(task);
+    } else{
+        res.status(404).send({message : 'Cannot find given id. '});
+    }
+}));
+
+app.delete('tasks/:id', asyncHandler(async (req,res)=>{
+    const id = req.params.id;
+    const idx =await Task.findByIdAndDelete(id);
+    if(idx >= 0) {
+        res.sendStatus(204);
+    } else{
+        res.status(404).send({message : 'Cannot find given id. '});
+    }
+}));
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
---
+++ models/Task.js
@@ -0,0 +1,32 @@
+
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+    {
+        title:{
+            type:String,
+            required: true,
+            maxLength: 30,
+            validata: {
+                validator : function (title) {
+                    return title.split(' ').length > 1;
+                },
+                message : 'Must contain at least 2 words',
+            },
+        },
+        description:{
+            type:String,
+        },
+        isComplete:{
+            type:Boolean,
+            default:false,
+        }
+    },
+    {
+        timestamps:true,
+    },
+);
+
+const Task = mongoose.model('Task',TaskSchema);
+
+export default Task;
